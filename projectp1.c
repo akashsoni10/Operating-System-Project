943a69d996f315a7d13c95e94c8efc0e97bb84fa
akashsoni10 <63649129+akashsoni10@users.noreply.github.com>
2020-04-14 20:04:27 +0530

    Add files via upload

--- projectp1.c
+++ projectp1.c
@@ -1,39 +1,19 @@
-// This Part Contain declaration of variables, array, calculation of waiting time and average waiting time
-
-#include<stdio.h>
-#include<conio.h>
-int j,i,k,n,arv[10],burst[10],wait[10],tat[10];
-void waitingtime();
-void avgwait();
-void tatm();
-void avgtat();
-void tottime();
-void waitingtime()
+void tatm()
 {
-	int temp=0;
-	printf("Waiting time of process p1 is:2");
-	for(i=1;i<n;i++)
-	{
-		
-	//	for(j=0;j<i;j++)
-	//	{
-			temp+=burst[i];
-			
-	//	}
-		temp+=arv[0];
-		printf("\nWaiting time of process p%d:%d",i+1,temp-arv[i]);
-		wait[i]=temp-arv[i];
-		temp=0;
-		
-	}
-}
-void avgwait()
-{
-	int avg=0;
 	for(i=0;i<n;i++)
 	{
-		avg+=wait[i];
+		printf("\nTurnAroundTime of the process p%d is:%d",i+1,wait[i]+burst[i]);
+		tat[i]=wait[i]+burst[i];
 	}
-	printf("\nAverage waiting time is:%d",avg/n);
+	
+}
+void avgtat()
+{
+	int	avg=0;
+	for(i=0;i<n;i++)
+	{
+		avg+=tat[i];
+	}
+	printf("\nAverage TAT is:%d",avg/n);
 	
 }
